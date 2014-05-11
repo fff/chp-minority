69bc175968ce878266a659a1eb7c16305cee5710
fff <milel.f@gmail.com>
2014-05-11 21:04:58 +0800

    fff - scaffold of mino web site

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+'use strict';
+module.exports = function(grunt) {
+	grunt.initConfig({
+    	clean: {
+    		views: ["dist/**/*.html"],
+    		styles: ['dist/assets/css'],
+    		release: ["dist/assets"]
+    	},
+    	
+    	copy: {
+			assets: {
+				files: [
+					{expand: true, cwd: 'bower_components/bootstrap/dist/', src: ['**'], dest: 'dist/assets/bootstrap/'},
+					{expand: true, cwd: 'bower_components/jquery/dist/', src: ['**'], dest: 'dist/assets/jquery/'}
+				]
+			}
+    	},
+
+		jade: {
+	        compile: {
+	            options: {
+	            	client: false,
+	                pretty: true,
+	                wrap: 'amd'
+	            },
+	            files: grunt.file.expandMapping(['**/*.jade'], 'dist/', 
+	            	{
+	            		cwd: 'src/views', rename: function(destBase, destPath) {
+                    		return destBase + destPath.replace(/\.jade$/, '.html'); } } )
+	        }
+    	},
+
+    	less: {
+    		dev: {
+    			files: grunt.file.expandMapping(['**/*.less'], 'dist/assets/css/', 
+	            	{
+	            		cwd: 'src/styles', rename: function(destBase, destPath) {
+                    		return destBase + destPath.replace(/\.less$/, '.css'); } } )
+	        	}
+    	},
+    	
+    	watch: {
+    		views: {
+    			files: 'src/views/**/*.jade',
+    			tasks: ['clean:views', 'jade']
+    		},
+    		styles: {
+    			files: 'src/styles/**/*.less',
+    			tasks: ['clean:styles', 'less']
+    		},
+    		reload: {
+    			files: 'dist/**/*',
+    			options: {
+    				livereload: true
+    			}
+    		}
+    	}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	
+	grunt.registerTask('default', ['clean', 'copy', 'jade', 'watch']);
+
+};
